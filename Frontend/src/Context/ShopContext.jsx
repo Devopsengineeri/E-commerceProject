6c3e0dc4f08d56f3269c45daf5d47a90ed3f1af7
sumit <s.k7527963527@gmail.com>
2024-12-30 16:02:22 +0530

    thired commit

--- Frontend/src/Context/ShopContext.jsx
+++ Frontend/src/Context/ShopContext.jsx
@@ -43,6 +43,27 @@ export const ShopContextProvider = (props) => {
     return totalCount;
   };
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  };
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+    return totalAmount;
+  };
+
   const value = {
     products,
     currency,
@@ -54,6 +75,8 @@ export const ShopContextProvider = (props) => {
     cartItems,
     addToCart,
     getCartCount,
+    updateQuantity,
+    getCartAmount,
   };
 
   useEffect(() => {
--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -81,7 +81,7 @@ const Product = () => {
             </div>
           </div>
           <button
-            onClick={() => addToCart(productData._id, size)}
+            onClick={() => addToCart(productData._id, size, productData)}
             className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
           >
             ADD TO CART
